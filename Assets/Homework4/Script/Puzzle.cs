ac4590fe042866e40d92bc496fb86911dc144bf0
Vyacheslav <Indoril.v@ya.ru>
2020-11-09 16:43:46 +0300

    Homework4

---
+++ Assets/Homework4/Script/Puzzle.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Puzzle : MonoBehaviour
+{
+    private PuzzleItem[] _items;
+    
+    void Start()
+    {
+        _items = gameObject.GetComponentsInChildren<PuzzleItem>();
+    }
+
+    public void Trigger(int id)
+    {
+        bool isAllChanged = true;
+        foreach (PuzzleItem item in _items)
+        {
+            if (item.Id == id)
+            {
+                item.SetEnabled(true);
+            }
+
+            if (!item.isChanged())
+            {
+                isAllChanged = false;
+            }
+        }
+        
+        if (isAllChanged)
+        {
+            Win();
+        }
+    }
+
+    public void Win()
+    {
+        //todo выдать ключ
+    }
+}
